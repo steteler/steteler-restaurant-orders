d4ea9dd79d1ecfe7ddd816dce6d60048fbc57b92
kikuti <joaovictorkikuti@gmail.com>
2023-05-29 00:13:44 -0300

    terminado: requisito 1 e 2

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,42 @@
 from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
+import pytest
 
 
 # Req 2
 def test_dish():
-    pass
+    with pytest.raises(TypeError):
+        Dish("miojo", "biscoito")
+
+    with pytest.raises(ValueError):
+        Dish("miojo", 0)
+
+    dish = Dish("miojo", 8.00)
+    dish2 = Dish("miojo", 8.00)
+    dish3 = Dish("arroz", 10.00)
+
+    assert repr(dish) == "Dish('miojo', R$8.00)"
+    assert dish.name == "miojo"
+
+    assert dish.__eq__(dish2) is True
+    assert dish.__eq__(dish3) is False
+
+    assert hash(dish) == hash(dish2)
+    assert hash(dish) != hash(dish3)
+
+    ingredient1 = Ingredient("salmão")
+    ingredient2 = Ingredient("camarão")
+
+    dish.add_ingredient_dependency(ingredient1, 2)
+    dish.add_ingredient_dependency(ingredient2, 1)
+
+    assert dish.get_ingredients() == {
+        Ingredient("salmão"),
+        Ingredient("camarão"),
+    }
+
+    assert dish.get_restrictions() == {
+        Restriction.ANIMAL_MEAT,
+        Restriction.SEAFOOD,
+        Restriction.ANIMAL_DERIVED,
+    }
--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,17 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient1 = Ingredient("morango")
+    ingredient2 = Ingredient("paçoca")
+
+    assert hash(ingredient1) == hash("morango")
+    assert hash(ingredient1) != hash(ingredient2)
+
+    assert ingredient1 == ingredient1
+    assert ingredient1 != ingredient2
+
+    assert repr(ingredient1) == "Ingredient('morango')"
+
+    assert ingredient1.name == "morango"
+
+    assert ingredient1.restrictions == set()
